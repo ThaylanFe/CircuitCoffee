76edaccb29fbfbe78679ec8e37b830dcb7a1b8d3
ThaylanFeh <felipethaylan311@gmail.com>
2024-06-21 03:48:10 -0300

    comentários

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,61 @@
+// Importações necessárias
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:intl/intl.dart';
 
+// Constantes para a URL e chave de acesso do Supabase
 const supabaseUrl = 'https://pouexwwhwxnuzoxvnejb.supabase.co';
 const supabaseKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBvdWV4d3dod3hudXpveHZuZWpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxNjgyMzEsImV4cCI6MjAzMDc0NDIzMX0.X6VmW9b-dtkKHAugIpN9U_yFaB4evhOaZ0cMVVsMlgs';
 
+// Função principal do aplicativo
 Future<void> main() async {
+  // Inicializa a conexão com o Supabase
   await Supabase.initialize(
     url: supabaseUrl,
     anonKey: supabaseKey,
   );
+  // Inicializa o aplicativo Flutter
   runApp(MyApp());
 }
 
+// Classe do aplicativo
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Define o tema do aplicativo
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Circuit Coffee',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      // Define AddPedidoScreen como tela inicial
       home: AddPedidoScreen(),
     );
   }
 }
 
+// Classe da tela de adicionar pedido
 class AddPedidoScreen extends StatefulWidget {
   @override
   _AddPedidoScreenState createState() => _AddPedidoScreenState();
 }
 
+// Classe do estado da tela de adicionar pedido
 class _AddPedidoScreenState extends State<AddPedidoScreen> {
+  // Controladores para os campos de texto
   TextEditingController _clienteController = TextEditingController();
   TextEditingController _valorController = TextEditingController();
   TextEditingController _obsPedController = TextEditingController();
 
+  // Variáveis para armazenar os valores selecionados nos dropdowns
   String _bebida = 'Frappucino';
   String _acompanhamento = 'Salgado';
 
   @override
   void dispose() {
+    // Dispose dos controladores para liberar recursos
     _clienteController.dispose();
     _valorController.dispose();
     _obsPedController.dispose();
@@ -51,20 +64,23 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Estrutura da tela com duas colunas
     return Scaffold(
       body: Row(
         children: [
+          // Coluna da esquerda com imagem
           Expanded(
             child: Container(
               color: Colors.purple[800],
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Image.asset('assets/images/2.png'),
+                  Image.asset('assets/images/2.png'), // Exibe uma imagem
                 ],
               ),
             ),
           ),
+          // Coluna da direita com formulário
           Expanded(
             child: Container(
               color: Colors.black,
@@ -73,6 +89,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    // Campo de texto para o nome do cliente
                     TextField(
                       controller: _clienteController,
                       decoration: InputDecoration(
@@ -87,6 +104,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                       ),
                       style: TextStyle(color: Colors.white),
                     ),
+                    // Dropdown para selecionar bebida
                     DropdownButtonFormField<String>(
                       value: _bebida,
                       decoration: InputDecoration(
@@ -116,6 +134,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                           .toList(),
                       onChanged: (value) => setState(() => _bebida = value!),
                     ),
+                    // Dropdown para selecionar acompanhamento
                     DropdownButtonFormField<String>(
                       value: _acompanhamento,
                       decoration: InputDecoration(
@@ -146,6 +165,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                       onChanged: (value) =>
                           setState(() => _acompanhamento = value!),
                     ),
+                    // Campo de texto para o valor do pedido
                     TextField(
                       controller: _valorController,
                       decoration: InputDecoration(
@@ -161,6 +181,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                       style: TextStyle(color: Colors.white),
                       keyboardType: TextInputType.number,
                     ),
+                    // Campo de texto para observações
                     TextField(
                       controller: _obsPedController,
                       decoration: InputDecoration(
@@ -175,6 +196,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                       ),
                       style: TextStyle(color: Colors.white),
                     ),
+                    // Dropdown para status do pedido
                     DropdownButtonFormField<String>(
                       value: 'Aguardando',
                       decoration: InputDecoration(
@@ -203,8 +225,10 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                       onChanged: (value) => print(value),
                     ),
                     SizedBox(height: 16),
+                    // Botão para adicionar pedido
                     ElevatedButton(
                       onPressed: () async {
+                        // Chama a função para adicionar pedido
                         await _addPedido(
                           _clienteController.text,
                           _bebida,
@@ -212,6 +236,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
                           double.tryParse(_valorController.text) ?? 0.0,
                           _obsPedController.text,
                         );
+                        // Exibe mensagem de confirmação
                         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
                           content: Text('Pedido adicionado com sucesso!'),
                         ));
@@ -238,6 +263,7 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
     );
   }
 
+  // Função para adicionar pedido
   Future<void> _addPedido(
     String nomeCliente,
     String bebida,
@@ -245,11 +271,14 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
     double valor,
     String obsPed,
   ) async {
+    // Obtém a data atual e formata-a
     final now = DateTime.now();
     final formattedDate = DateFormat('yyyy-MM-dd').format(now);
 
+    // Obtém o último ID de pedido
     final lastId = await _getLastId();
 
+    // Cria um mapa com os dados do pedido
     final pedido = {
       'id': lastId,
       'Bebida': bebida,
@@ -261,9 +290,11 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
       'NomeCli': nomeCliente,
     };
 
+    // Insere o pedido no Supabase
     final response =
         await Supabase.instance.client.from('core_pedido').insert([pedido]);
 
+    // Trata erros de inserção
     if (response == null) {
       print('Erro ao adicionar pedido:Unknown error');
       return;
@@ -275,14 +306,19 @@ class _AddPedidoScreenState extends State<AddPedidoScreen> {
     }
   }
 
+  // Função para obter o último ID de pedido
   Future<int> _getLastId() async {
+    // Obtém o último ID de pedido do Supabase
     final response = await Supabase.instance.client
         .from('core_pedido')
         .select('id')
         .order('id', ascending: false)
         .limit(1);
 
+    // Verifica se há registros e obtém o último ID
     final lastId = response.isNotEmpty ? response[0]['id'] : 1;
+
+    // Retorna o próximo ID disponível
     return lastId + 1;
   }
 }
